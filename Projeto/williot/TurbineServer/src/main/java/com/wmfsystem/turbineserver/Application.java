e6cfb2603e8f99363ccfc174155c6d7f915f7781
willian <willianmarquesfreire@gmail.com>
2017-04-18 14:12:02 -0300

    dashboard turbine

---
+++ Projeto/williot/TurbineServer/src/main/java/com/wmfsystem/turbineserver/Application.java
@@ -0,0 +1,22 @@
+package com.wmfsystem.turbineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wmfsystem on 4/15/17.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
