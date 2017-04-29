5ff36f5385968bcf066b76ec4215198b963542b6
wmfsystem <willianmarquesfreire@gmail.com>
2017-04-29 16:54:13 -0300

    melhorias

---
+++ Projeto/IoT/testes/IoTEurekaClient.lua
@@ -0,0 +1,61 @@
+function get(address, decode, callback)
+    print("Send to address " .. address)
+    http.get(
+        address,
+        "Accept: application/json\r\n",
+        function(code, data)
+            if (code < 0) then
+                print("HTTP request failed")
+            else
+                if (decode) then
+                    local resp = cjson.decode(data)
+                    callback(resp)
+                else
+                    callback(data)
+                end
+            end
+        end
+    )
+end
+
+function main()
+    get(
+        "http://192.168.25.26:8000/eureka/apps/muniserver",
+        true,
+        function(resp)
+            ipAplicacao = resp.application.instance.ipAddr
+            portAplicacao = resp.application.instance.port["$"]
+            print(ipAplicacao .. ":" .. portAplicacao)
+            get(
+                ipAplicacao .. ":" .. portAplicacao,
+                false,
+                function(app)
+                    print(app)
+                end
+            )
+        end
+    )
+end
+
+wifi.setmode(wifi.STATION)
+--wifi.sta.config("WiFi-Repeater", "willianfreire")
+wifi.sta.config("GUMGA", "gumgaqwe123")
+wifi.sta.connect()
+
+timeout = 0
+tmr.alarm(
+    1,
+    1000,
+    1,
+    function()
+        if (wifi.sta.getip() == nil) then
+            timeout = timeout + 1
+            print("Trying to connect!")
+        end
+        if (wifi.sta.getip() ~= nil) then
+            tmr.stop(1)
+            print("Connected. IP is " .. wifi.sta.getip())
+            main()
+        end
+    end
+)
--- Projeto/IoT/testes/auto wifi/init.lua
+++ Projeto/IoT/testes/auto wifi/init.lua
@@ -1,61 +1,76 @@
 --Pedro Minatel - 2015-12-13
 print("Starting...")
 
-if pcall(function ()
-    print("Open config")
-    dofile("config.lc")
-end) then
-   print("Connecting to WIFI...")
-   --Configuring WiFi as STATION
-   wifi.setmode(wifi.STATION)
-   wifi.sta.config("WiFi-Repeater","willianfreire")
-   wifi.sta.connect()
-   timeout = 0;
-   tmr.alarm(1, 1000, 1, function()
-      if wifi.sta.getip() == nil then
-         print("IP unavaiable, waiting... " .. timeout)
-      timeout = timeout + 1
-      if timeout >= 60 then
-       file.remove('config.lc')
-       node.restart()
-      end
-      else
-         tmr.stop(1)
-         print("Connected, IP is "..wifi.sta.getip())
-        --do your code here
-      end
-   end)
+if pcall(
+    function()
+        print("Open config")
+        dofile("config.lc")
+    end
+) then
+    print("Connecting to WIFI...")
+    --Configuring WiFi as STATION
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config("WiFi-Repeater", "willianfreire")
+    wifi.sta.connect()
+    timeout = 0
+    tmr.alarm(
+        1,
+        1000,
+        1,
+        function()
+            if wifi.sta.getip() == nil then
+                print("IP unavaiable, waiting... " .. timeout)
+                timeout = timeout + 1
+                if timeout >= 60 then
+                    file.remove('config.lc')
+                    node.restart()
+                end
+            else
+                --do your code here
+                tmr.stop(1)
+                print("Connected, IP is " .. wifi.sta.getip())
+            end
+        end
+    )
 else
-   print("Enter configuration mode")
-   dofile("wifi_setup.lua")
-end--Pedro Minatel - 2015-12-13
+    print("Enter configuration mode")
+    dofile("wifi_setup.lua")
+end
+ --Pedro Minatel - 2015-12-13
 print("Starting...")
 
-if pcall(function ()
-    print("Open config")
-    dofile("config.lc")
-end) then
-   print("Connecting to WIFI...")
-   --Configuring WiFi as STATION
-   wifi.setmode(wifi.STATION)
-   wifi.sta.config("WiFi-Repeater","willianfreire")
-   wifi.sta.connect()
-   timeout = 0;
-   tmr.alarm(1, 1000, 1, function()
-      if wifi.sta.getip() == nil then
-         print("IP unavaiable, waiting... " .. timeout)
-      timeout = timeout + 1
-      if timeout >= 60 then
-       file.remove('config.lc')
-       node.restart()
-      end
-      else
-         tmr.stop(1)
-         print("Connected, IP is "..wifi.sta.getip())
-        --do your code here
-      end
-   end)
+if pcall(
+    function()
+        print("Open config")
+        dofile("config.lc")
+    end
+) then
+    print("Connecting to WIFI...")
+    --Configuring WiFi as STATION
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config("WiFi-Repeater", "willianfreire")
+    wifi.sta.connect()
+    timeout = 0
+    tmr.alarm(
+        1,
+        1000,
+        1,
+        function()
+            if wifi.sta.getip() == nil then
+                print("IP unavaiable, waiting... " .. timeout)
+                timeout = timeout + 1
+                if timeout >= 60 then
+                    file.remove('config.lc')
+                    node.restart()
+                end
+            else
+                --do your code here
+                tmr.stop(1)
+                print("Connected, IP is " .. wifi.sta.getip())
+            end
+        end
+    )
 else
-   print("Enter configuration mode")
-   dofile("wifi_setup.lua")
+    print("Enter configuration mode")
+    dofile("wifi_setup.lua")
 end
